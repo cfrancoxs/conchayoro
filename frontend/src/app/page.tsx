14c0fbd21c6ceece6387aabec8f2a1b192a8b8b3
cfrancoxs <1496404@sga.pucminas.br>
2024-08-24 21:27:30 +0000

    cria app next.js

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 max-w-5x1 w-full items-center justify-between font-monotext-sm
+lg:flex">
+      <h1> Welcome to the ConchaYoro App </h1>
+      </div>
+    </main>
+  )
+}
